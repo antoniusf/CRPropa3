4a36684dbc6369ed25e211f9523335106befda2f
Antonius Frie <antonius.frie@rub.de>
2019-05-17 15:20:58 +0200

    Make an SSE version again

--- src/magneticField/TD13Field.cpp
+++ src/magneticField/TD13Field.cpp
@@ -61,40 +61,11 @@ namespace crpropa {
   }
 
 #ifdef FAST_TD13
-  //see https://stackoverflow.com/questions/49941645/get-sum-of-values-stored-in-m256d-with-sse-avx
-  double hsum_double_avx(__m256d v) {
-    __m128d vlow  = _mm256_castpd256_pd128(v);
-    __m128d vhigh = _mm256_extractf128_pd(v, 1); // high 128
-    vlow  = _mm_add_pd(vlow, vhigh);     // reduce down to 128
 
-    __m128d high64 = _mm_unpackhi_pd(vlow, vlow);
-    return  _mm_cvtsd_f64(_mm_add_sd(vlow, high64));  // reduce to scalar
-  }
-
-  // code for hsum_float_avx taken from:
-  // https://stackoverflow.com/questions/13219146/how-to-sum-m256-horizontally
-
-  // x = ( x7, x6, x5, x4, x3, x2, x1, x0 )
-  float hsum_float_avx(__m256 x) {
-    // hiQuad = ( x7, x6, x5, x4 )
-    const __m128 hiQuad = _mm256_extractf128_ps(x, 1);
-    // loQuad = ( x3, x2, x1, x0 )
-    const __m128 loQuad = _mm256_castps256_ps128(x);
-    // sumQuad = ( x3 + x7, x2 + x6, x1 + x5, x0 + x4 )
-    const __m128 sumQuad = _mm_add_ps(loQuad, hiQuad);
-    // loDual = ( -, -, x1 + x5, x0 + x4 )
-    const __m128 loDual = sumQuad;
-    // hiDual = ( -, -, x3 + x7, x2 + x6 )
-    const __m128 hiDual = _mm_movehl_ps(sumQuad, sumQuad);
-    // sumDual = ( -, -, x1 + x3 + x5 + x7, x0 + x2 + x4 + x6 )
-    const __m128 sumDual = _mm_add_ps(loDual, hiDual);
-    // lo = ( -, -, -, x0 + x2 + x4 + x6 )
-    const __m128 lo = sumDual;
-    // hi = ( -, -, -, x1 + x3 + x5 + x7 )
-    const __m128 hi = _mm_shuffle_ps(sumDual, sumDual, 0x1);
-    // sum = ( -, -, -, x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 )
-    const __m128 sum = _mm_add_ss(lo, hi);
-    return _mm_cvtss_f32(sum);
+  // this is the second part of hsum_double_avx
+  double hsum_double_sse(__m128d v) {
+    __m128d high64 = _mm_unpackhi_pd(v, v);
+    return _mm_cvtsd_f64(_mm_add_sd(v, high64));
   }
 #endif // defined(FAST_TD13)
 
@@ -242,52 +213,52 @@ namespace crpropa {
     // of the accumulator's numbers are added together (using
     // hsum_double_avx), resulting in the total sum.
 
-    __m256d acc0 = _mm256_setzero_pd();
-    __m256d acc1 = _mm256_setzero_pd();
-    __m256d acc2 = _mm256_setzero_pd();
+    __m128d acc0 = _mm_setzero_pd();
+    __m128d acc1 = _mm_setzero_pd();
+    __m128d acc2 = _mm_setzero_pd();
 
     // broadcast position into SSE registers
-    __m256d pos0 = _mm256_set1_pd(pos.x);
-    __m256d pos1 = _mm256_set1_pd(pos.y);
-    __m256d pos2 = _mm256_set1_pd(pos.z);
+    __m128d pos0 = _mm_set1_pd(pos.x);
+    __m128d pos1 = _mm_set1_pd(pos.y);
+    __m128d pos2 = _mm_set1_pd(pos.z);
 
-    for (int i=0; i<avx_Nm; i+=4) {
+    for (int i=0; i<avx_Nm; i+=2) {
 
       // load data from memory into AVX registers
-      __m256d Axi0 = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*iAxi0);
-      __m256d Axi1 = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*iAxi1);
-      __m256d Axi2 = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*iAxi2);
+      __m128d Axi0 = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*iAxi0);
+      __m128d Axi1 = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*iAxi1);
+      __m128d Axi2 = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*iAxi2);
 
-      __m256d kkappa0 = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*ikkappa0);
-      __m256d kkappa1 = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*ikkappa1);
-      __m256d kkappa2 = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*ikkappa2);
+      __m128d kkappa0 = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*ikkappa0);
+      __m128d kkappa1 = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*ikkappa1);
+      __m128d kkappa2 = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*ikkappa2);
 
-      __m256d beta = _mm256_load_pd(avx_data.data() + i + align_offset + avx_Nm*ibeta);
+      __m128d beta = _mm_load_pd(avx_data.data() + i + align_offset + avx_Nm*ibeta);
 
 
       // Do the computation
 
       // this is the scalar product between k*kappa and pos
-      __m256d z = _mm256_add_pd(_mm256_mul_pd(pos0, kkappa0),
-			       _mm256_add_pd(_mm256_mul_pd(pos1, kkappa1),
-					     _mm256_mul_pd(pos2, kkappa2)
+      __m128d z = _mm_add_pd(_mm_mul_pd(pos0, kkappa0),
+			       _mm_add_pd(_mm_mul_pd(pos1, kkappa1),
+					     _mm_mul_pd(pos2, kkappa2)
 					     )
 			       );
 
       // here, the phase is added on. this is the argument of the cosine.
-      __m256d cos_arg = _mm256_add_pd(z, beta);
+      __m128d cos_arg = _mm_add_pd(z, beta);
 
       // ********
       // * Computing the cosine
       // *
       // * argument reduction
       // step 1: compute round(x), and store it in q
-      __m256d q = _mm256_round_pd(cos_arg, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
+      __m128d q = _mm_round_pd(cos_arg, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
 
       // we now compute s, which will be the input parameter to our polynomial approximation
       // of cos(pi/2*x) between 0 and 1
       // the andnot_pd is just a fast way of taking the absolute value
-      __m256d s = _mm256_sub_pd(cos_arg, q);
+      __m128d s = _mm_sub_pd(cos_arg, q);
     
       // the following based on the int extraction process described here:
       // https://stackoverflow.com/questions/41144668/how-to-efficiently-perform-double-int64-conversions-with-sse-avx/41223013
@@ -301,7 +272,7 @@ namespace crpropa {
       // since q may be negative, we'll also add 2^51 to make sure it's positive.
       // note that 2^51 is even and thus leaves evenness invariant, which is the only thing we care about here.
 
-      q = _mm256_add_pd(q, _mm256_set1_pd(0x0018000000000000));
+      q = _mm_add_pd(q, _mm_set1_pd(0x0018000000000000));
 
       // unfortunately, integer comparisons were only introduced in avx2, so we'll have to make do
       // with a floating point comparison to check whether the last bit is set.
@@ -311,13 +282,13 @@ namespace crpropa {
       // but also the exponent (and sign, but that doesn't matter) of q. Luckily, the exponent of q
       // is guaranteed to have the fixed value of 1075 (corresponding to 2^52) after our addition.
 
-      __m256d invert = _mm256_and_pd(q, _mm256_castsi256_pd(_mm256_set1_epi64x(0xfff0000000000001)));
+      __m128d invert = _mm_and_pd(q, _mm_castsi128_pd(_mm_set1_epi64x(0xfff0000000000001)));
 
       // if we did have a one in bit 0, our result will be equal to 2^52 + 1
-      invert = _mm256_cmp_pd(invert, _mm256_castsi256_pd(_mm256_set1_epi64x(0x4330000000000001)), _CMP_EQ_OQ);
+      invert = _mm_cmpeq_pd(invert, _mm_castsi128_pd(_mm_set1_epi64x(0x4330000000000001)));
 
       // finally, we need to turn invert and right_invert into masks for the sign bit on each final double, ie
-      invert = _mm256_and_pd(invert, _mm256_set1_pd(-0.0));
+      invert = _mm_and_pd(invert, _mm_set1_pd(-0.0));
 
       // TODO: clamp floats between 0 and 1? This would ensure that we never see inf's, but maybe we want that,
       // so that things dont just fail silently...
@@ -331,19 +302,19 @@ namespace crpropa {
       // * the coefficients for this were generated using sleefs gencoef.c
       // * I have no idea what I'm doing, so these coefficients are probably far from optimal.
       // * However, they should be sufficient for this case.
-      s = _mm256_mul_pd(s, s);
+      s = _mm_mul_pd(s, s);
 
-      __m256d u = _mm256_set1_pd(+0.2211852080653743946e+0);
+      __m128d u = _mm_set1_pd(+0.2211852080653743946e+0);
 
-      u = _mm256_add_pd(_mm256_mul_pd(u, s), _mm256_set1_pd(-0.1332560668688523853e+1 ));
-      u = _mm256_add_pd(_mm256_mul_pd(u, s), _mm256_set1_pd(+0.4058509506474178075e+1 ));
-      u = _mm256_add_pd(_mm256_mul_pd(u, s), _mm256_set1_pd(-0.4934797516664651162e+1));
-      u = _mm256_add_pd(_mm256_mul_pd(u, s), _mm256_set1_pd(1.));
+      u = _mm_add_pd(_mm_mul_pd(u, s), _mm_set1_pd(-0.1332560668688523853e+1 ));
+      u = _mm_add_pd(_mm_mul_pd(u, s), _mm_set1_pd(+0.4058509506474178075e+1 ));
+      u = _mm_add_pd(_mm_mul_pd(u, s), _mm_set1_pd(-0.4934797516664651162e+1));
+      u = _mm_add_pd(_mm_mul_pd(u, s), _mm_set1_pd(1.));
     
       // then, flip the sign of each double for which invert is not zero. since invert
       // has only zero bits except for a possible one in bit 63, we can xor it onto
       // our result to selectively invert the 63st (sign) bit in each double where invert is set.
-      u = _mm256_xor_pd(u, invert);
+      u = _mm_xor_pd(u, invert);
 
       // * end computation of cosine
       // **********
@@ -351,14 +322,14 @@ namespace crpropa {
       // Finally, Ak*xi is multiplied on. Since this is a vector, the
       // multiplication needs to be done for each of the three
       // components, so it happens separately.
-      acc0 = _mm256_add_pd(_mm256_mul_pd(u, Axi0), acc0);
-      acc1 = _mm256_add_pd(_mm256_mul_pd(u, Axi1), acc1);
-      acc2 = _mm256_add_pd(_mm256_mul_pd(u, Axi2), acc2);
+      acc0 = _mm_add_pd(_mm_mul_pd(u, Axi0), acc0);
+      acc1 = _mm_add_pd(_mm_mul_pd(u, Axi1), acc1);
+      acc2 = _mm_add_pd(_mm_mul_pd(u, Axi2), acc2);
   }
   
-  return Vector3d(hsum_double_avx(acc0),
-                  hsum_double_avx(acc1),
-                  hsum_double_avx(acc2)
+  return Vector3d(hsum_double_sse(acc0),
+                  hsum_double_sse(acc1),
+                  hsum_double_sse(acc2)
                   );
 #endif // FAST_TD13
 }
